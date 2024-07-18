67b703f1d19302fc2e2c841916a495627eb5d837
NatsuRohit <rohitprajapati2077@gmail.com>
2024-07-17 20:51:36 -0700

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,60 @@
+# class calculator:
+#     def add(self,x,y):
+#         return x+y
+    
+#     def sub(self,x,y):
+#         return x-y
+    
+#     def mul(self,x,y):
+#         return x*y
+    
+#     def div(self,x,y):
+#         return x/y
+    
+# obj=calculator()
+# print(obj.add(10,20))
+# print(obj.sub(10,20))
+# print(obj.mul(10,20))
+# print(obj.div(20,10))
+
+
+# class User:
+#     name='ram'
+
+#     def info(self):
+#         print(self.name)
+
+# obj=User()
+# obj.info()
+
+
+# class SIP():
+#     def take_value(self):
+#         p=int(input("Enter the principal:-\t"))
+#         t=int(input("Enter the time:-\t"))
+#         r=int(input("Enter the rate:-\t"))
+#         return [p,t,r]
+
+#     def calculate(self):
+#         p,t,r=self.take_value()
+#         return p*t*r/100
+
+#     def display(self):
+#         print(self.calculate())
+
+# obj=SIP()
+# obj.display()
+
+
+class product:
+    productData=['laptop','mobile','tv']
+    def add(self,new_product):
+        pass
+    def show(self):
+        pass
+
+    def update(self,old_product,new_product):
+        pass
+
+    def delete(self,product):
+        pass
